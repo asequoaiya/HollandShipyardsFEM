7545b47e858d415746924d4551c9e38d46e72e15
KevinLu <kevin.lu05@gmail.com>
2024-02-07 14:31:57 +0100

    Added the correct inputs in InputParams.py. Also added currently deprecated entries for stiffeners and stringers, although I'm unsure of how those interact with the impact.

--- DeflectionEstimation/InputParams.py
+++ DeflectionEstimation/InputParams.py
@@ -1,11 +1,51 @@
-# ----- Import libraries -----
-import numpy as np
+# ----- Import library -----
+from math import radians
 
 # ----- Geometry constants -----
 # --- Plate ---
 length = 0.8  # [m]
 width = 0.6  # [m]
 thickness = 0.01  # [m]
+
+area = length * width  # [m2]
+
+# # --- Stiffeners ---
+# stiffener_thickness = 0.009  # [m]
+# stiffener_web_height = 0.175  # [m]
+# stiffener_spacing = 0.6  # [m]
+#
+# # --- Stringers ---
+# stringer_web_thickness = 0.008  # [m]
+# stringer_web_height = 0.1  # [m]
+# stringer_flange_thickness = 0.015  # [m]
+# stringer_flange_height = 0.1  # [m]
+# stringer_spacing = 0.8  # [m]
+
+# ----- Scenario constants -----
+hit_location = [50, 50]  # % of length
+a_1 = length * hit_location[0] / 100  # [m]
+a_2 = length * (100 - hit_location[0]) / 100  # [m]
+
+b_1 = length * hit_location[1] / 100  # [m]
+b_2 = length * (100 - hit_location[1]) / 100  # [m]
+
+phi = radians(96.36)  # [deg -> rad]
+
+# ----- Other constants -----
+flow_stress = 297.5 * 10 ** 6  # [Pa]
+friction_coefficient = 0.3  # [-]
+
+
+# ----- Input function -----
+def energy_input():
+    input_list = [a_1, a_2, b_1, b_2,
+                  flow_stress, thickness, area, friction_coefficient, phi]
+
+    return input_list
+
+
+
+
 
 
 
