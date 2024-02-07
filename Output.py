f44778697a84625785b41d121821e7e987b40dfe
KevinLu <kevin.lu05@gmail.com>
2024-02-07 10:32:27 +0100

    Added start, end and step size parameters in impact_angle_scenario. Also added a numerical text output.

--- Output.py
+++ Output.py
@@ -3,20 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # ----- Import functions -----
-from math import radians
+from math import radians, degrees
 import InputParams
 import EnergyCalc
 
 
 # ----- Increment function -----
-def impact_angle_scenario(graphing=True):
+def impact_angle_scenario(start, end, step_size,
+                          text_output=True, graphing=True):
     """
     Runs the simulation for a range of different angles.
     """
 
     # Empty array to store data
-    energy_array = np.zeros(151)
-    angle_array = np.arange(151) + 15
+    energy_array = np.zeros(int((end - start) / step_size) + 1)
+    angle_array = (np.arange(start, end + step_size, step_size)
+                   + degrees(InputParams.beta))
 
     # Set of angles (15-165 degrees)
     for n in range(len(energy_array)):
@@ -28,14 +30,21 @@ def impact_angle_scenario(graphing=True):
         energy_array[n] = energy_loss
 
         # Iterate through beta
-        InputParams.beta += radians(1)
+        InputParams.beta += radians(step_size)
+
+    if text_output:
+        max_energy_location = list(energy_array).index(max(energy_array))
+        print(f"The maximum amount of lost energy is {max(energy_array)} MJ.")
+        print(f"This is at an impact angle of "
+              f"{angle_array[max_energy_location]} degrees.")
 
     if graphing:
         plt.plot(angle_array, energy_array)
         plt.xlabel("Impact angle beta [deg]")
         plt.ylabel("Energy loss [MJ]")
+        plt.ylim(0, 4.5)
         plt.grid()
         plt.show()
 
 
-impact_angle_scenario()
+impact_angle_scenario(0, 150, 0.1)
