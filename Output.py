7b33b09fd21cd8cfbc7024211bb4ae83ce7de2ff
KevinLu <kevin.lu05@gmail.com>
2024-02-07 09:32:50 +0100

    Added graphing argument.

--- Output.py
+++ Output.py
@@ -1,21 +1,41 @@
-# ----- Import variables -----
+# ----- Import packages -----
 import numpy as np
+import matplotlib.pyplot as plt
+
+# ----- Import functions -----
+from math import radians
 import InputParams
 import EnergyCalc
-from math import radians, degrees
 
 
 # ----- Increment function -----
-def increment():
-    # For
-    domain = np.arange(16, 166, 1)
-    for n in domain:
-        InputParams.beta += radians(1)
-        angle = int(degrees(InputParams.beta))
+def impact_angle_scenario(graphing=True):
+    """
+    Runs the simulation for a range of different angles.
+    """
 
+    # Empty array to store data
+    energy_array = np.zeros(151)
+    angle_array = np.arange(151) + 15
+
+    # Set of angles (15-165 degrees)
+    for n in range(len(energy_array)):
+        # Calculate energy loss in MJ
         energy_loss = (int(EnergyCalc.total_energy(*EnergyCalc.energy_input()))
                        / 10 ** 6)
-        print(angle, energy_loss, "MJ")
+
+        # Store data
+        energy_array[n] = energy_loss
+
+        # Iterate through beta
+        InputParams.beta += radians(1)
+
+    if graphing:
+        plt.plot(angle_array, energy_array)
+        plt.xlabel("Impact angle beta [deg]")
+        plt.ylabel("Energy loss [MJ]")
+        plt.grid()
+        plt.show()
 
 
-increment()
+impact_angle_scenario()
