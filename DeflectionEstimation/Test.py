a22a33c18a2bd26837773cf7b31f540dfd8b1ee0
kevin <kevin.lu05@gmail.com>
2024-02-08 10:01:16 +0100

    Changed testing variables

--- DeflectionEstimation/Test.py
+++ DeflectionEstimation/Test.py
@@ -4,21 +4,25 @@ from math import cos, sin
 from math import radians as rad
 
 # Test
-deltas = np.arange(0.1, 0.51, 0.01)
-phi = rad(35)
+delta = 0.5
+angles = np.arange(0, 91)
 
-for n, delta in enumerate(deltas):
-    numerator = delta * (1.2 * 1.2 + (delta * cos(phi)) ** 2)
-    denominator = ((1.2 + delta * cos(phi)) ** 2
-                   * (1.2 - delta * cos(phi)) ** 2)
+for n, angle in enumerate(angles):
+    phi = rad(angle)
 
-    second_term = sin(phi) ** 2 + sin(phi) * cos(phi)
+    var = sin(phi) ** 2 + sin(phi) * cos(phi)
+    print(var, angle)
+
+    # numerator = delta * (1.2 * 1.2 + (delta * cos(phi)) ** 2)
+    # denominator = ((1.2 + delta * cos(phi)) ** 2
+    #                * (1.2 - delta * cos(phi)) ** 2)
     #
-    print(numerator, denominator, numerator / denominator)
+    # second_term = sin(phi) ** 2 + sin(phi) * cos(phi)
+    # #
+    # print(numerator, denominator, numerator / denominator)
     # print(second_term)
 
     # print(second_term * numerator / denominator)
-
 
 
 
