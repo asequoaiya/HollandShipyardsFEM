fcd270e477cf49760aad7be2bfa405e6c8402789
kevin <kevin.lu05@gmail.com>
2024-02-12 09:48:21 +0100

    Fixed geometry term; accidentally used a double width. Now the results make more sense.

--- DeflectionEstimation/PlateStripMethod.py
+++ DeflectionEstimation/PlateStripMethod.py
@@ -7,7 +7,7 @@ def plate_strip_deflection(thickness, flow_stress, n, deflection,
 
     constant_term = 1 / (3 ** 0.5) * thickness * flow_stress
     n_term = n ** 2 / (2 * n - 1)
-    geometry_term = 2 * width / a_1 + 2 * width / a_2
+    geometry_term = width / a_1 + width / a_2
 
     energy_loss = constant_term * n_term * geometry_term * deflection ** 2
 
@@ -16,4 +16,3 @@ def plate_strip_deflection(thickness, flow_stress, n, deflection,
 
 print(plate_strip_deflection(0.01, 300_000_000, 1, 0.1896,
                              0.6, 0.3, 0.3))
-
