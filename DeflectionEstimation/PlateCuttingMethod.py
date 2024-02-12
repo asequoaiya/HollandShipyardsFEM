8e04a4bf023c7b9cfc38ea97ae61a54118db7a36
kevin <kevin.lu05@gmail.com>
2024-02-12 15:26:17 +0100

    Added the deck-cutting method.

---
+++ DeflectionEstimation/PlateCuttingMethod.py
@@ -0,0 +1,23 @@
+# ----- Import functions -----
+from DeflectionCalc import cos_p
+from numpy import tan
+from numpy import radians as rad
+
+
+def plate_cutting_deflection(flow_stress, ductility, mu,
+                             thickness, length, theta):
+    """
+    Energy calculation for the cutting of a plate.
+    """
+
+    material_term = 1.057 * flow_stress * ductility ** 0.25
+    geometry_term = thickness ** 1.25 * length ** 1.25 * cos_p(theta, -0.5)
+    friction_term = 1 + 0.5 * mu / tan(rad(theta))
+
+    energy_loss = material_term * geometry_term * friction_term
+
+    return energy_loss
+
+
+print(plate_cutting_deflection(300_000_000, 0.1, 0.6, 0.01, 0.485, 45))
+
