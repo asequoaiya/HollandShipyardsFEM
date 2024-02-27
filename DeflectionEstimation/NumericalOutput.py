1d48f8b690040ed75b7c6000e2e224b4094264b0
KevinLu <kevin.lu05@gmail.com>
2024-02-27 15:58:29 +0100

    Adjusted output.

--- DeflectionEstimation/NumericalOutput.py
+++ DeflectionEstimation/NumericalOutput.py
@@ -6,13 +6,13 @@ from PlateCuttingMethod import plate_cutting_energy
 
 
 # ----- Output calling -----
-print(plate_deflection_energy(1.5, 1.5, 0.358, 0.472, 300_000_000,
-                              0.025, 0.6, 90, 0.075))
+# print(plate_deflection_energy(0.5, 0.5, 0.358, 0.472, 300_000_000,
+                              # 0.025, 0.6, 90, 0.075))
 
-print(plate_strip_energy(0.01, 300_000_000, 1, 0.1896,
-                         0.6, 0.4, 0.4))
+# print(plate_strip_energy(0.01, 300_000_000, 1, 0.1896,
+                         # 0.6, 0.4, 0.4))
 
-print(deck_plate_crushing_energy(0.025, 300_000_000, 1, 0.1896,
-                                 0.8, 0.3, 0.3))
+print(deck_plate_crushing_energy(0.01, 300_000_000, 1, 0.1896,
+                                 3, 0.5, 0.5))
 
-print(plate_cutting_energy(300_000_000, 0.1, 0.6, 0.01, 0.485, 45))
+# print(plate_cutting_energy(300_000_000, 0.1, 0.6, 0.01, 0.485, 45))
