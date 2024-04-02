d72c97d01f6a50e8b22372b70931d92de430cc87
KevinLu <kevin.lu05@gmail.com>
2024-04-02 11:52:21 +0200

    Added a simple run file to run the processing.

---
+++ LSDYNAProcessing/Run.py
@@ -0,0 +1,6 @@
+# ----- Import function -----
+from EnergyCalculation import all_contact_energy
+
+all_contact_energy((r'C:\Users\kevin\OneDrive\Documents\HollandShipyards'
+                    r'\DNV RP C208\LS-DYNA Files\Full Ship\MillimeterModel'
+                    r'\WiderMMModel\BoxImpact'))
